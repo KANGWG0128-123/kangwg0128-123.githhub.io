948ed8d0180d2b5afd778df562cec1985f740d19
KANGWG0128-123 <kangwg0128@gmail.com>
2026-04-26 13:55:06 +0000

    Deploying to gh-pages from @ KANGWG0128-123/kangwg0128-123.githhub.io@7af07ae905a3b06fe6ec7572297f58bb0a6335f3 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://minjunkang.xyz/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://kangwg0128-123.github.io/#contact">contact us</a>.'
               }
             ]
           }
